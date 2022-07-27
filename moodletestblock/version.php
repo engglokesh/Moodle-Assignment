13750e1fd5a5141f99ff175f6cbaf946fb8fc9e9
engglokesh <engg.lokeshmalpani@gmail.com>
2022-07-27 13:12:07 +0530

    Course activities along with status
    Course activities along with status

---
+++ moodletestblock/version.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/**
+ * Version info
+ *
+ * @package    block_moodletestblock
+ * @copyright  2022 
+ * @author     Lokesh Malpani <engg.lokeshmalpani@gmail.com>
+
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version      = 2022041900; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires     = 2019111800; // Requires this Moodle version.
+$plugin->component    = 'block_moodletestblock';
+
